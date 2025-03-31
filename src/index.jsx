94c69476548a76723948085da10742971515f1fe
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-03-31 17:01:53 +0200

    added DisplayFAct function that I invoke as SJX element under render method

--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,22 @@ import {createRoot} from "react-dom/client" //remember it is called createRoot !
 
 const root = createRoot(document.getElementById("root"))
 
-root.render(<h1>hello you</h1>) //no citation marks in the render method
+
+function DisplayFacts () {
+    return(
+    <main>
+        <img src="src/assets/react.svg" width="40px" alt="React logo" />
+        <h1>Fun facts about React!</h1>
+        <ul>
+            <li>Was first release in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 200K stars on GitHub</li>
+            <li>Is maintained by Meta</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </main>
+    )
+}
+
+root.render(<DisplayFacts />) //no citation marks in the render method
+
