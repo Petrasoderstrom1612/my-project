3fa3f2ef8f36f1e56ca782e6ebdd77a2f4a53a5c
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-03-31 14:07:55 +0200

    changed repo name

--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,4 @@ import {createRoot} from "react-dom/client" //remember it is called createRoot !
 
 const root = createRoot(document.getElementById("root"))
 
-root.render(<h1>hello</h1>) //no citation marks in the render method
+root.render(<h1>hello you</h1>) //no citation marks in the render method
