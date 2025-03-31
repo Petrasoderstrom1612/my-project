34ec85e0979a3064089f0d627a2f60fd77e3acea
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-03-31 14:01:03 +0200

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import {createRoot} from "react-dom/client" //remember it is called createRoot !
+
+const root = createRoot(document.getElementById("root"))
+
+root.render(<h1>hello</h1>) //no citation marks in the render method
