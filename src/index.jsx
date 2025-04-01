309084a10f5d479e9728b814d72cc32ffafa2f6d
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-04-01 12:02:33 +0200

    added className to the JSX components. It can only be in the components, not in the invocations

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,43 @@
-import {createRoot} from "react-dom/client" //remember it is called createRoot !
+import { createRoot } from "react-dom/client"; //remember it is called createRoot !
 
-const root = createRoot(document.getElementById("root"))
+const root = createRoot(document.getElementById("root"));
 
-
-function DisplayFacts () {
-    return(
-    <main>
-        <img src="src/assets/react.svg" width="40px" alt="React logo" />
-        <h1>Fun facts about React!</h1>
-        <ul>
-            <li>Was first release in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 200K stars on GitHub</li>
-            <li>Is maintained by Meta</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="src/assets/react.svg" className="logo" alt="React logo" />
+      <nav>
+        <ul className="nav-list">
+          <li className="nav-list-item">Pricing</li>
+          <li className="nav-list-item">About</li>
+          <li className="nav-list-item">Contact</li>
         </ul>
-    </main>
-    )
+      </nav>
+    </header>
+  );
+};
+
+function DisplayFacts() {
+  return (
+    <>
+      <main className="main">
+        <Header />
+        <ul>
+          <li>Was first release in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 200K stars on GitHub</li>
+          <li>Is maintained by Meta</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+      </main>
+      <footer>
+        <small>
+          © {new Date().getFullYear()} Söderström development. All rights
+          reserved.
+        </small>
+      </footer>
+    </>
+  );
 }
 
-root.render(<DisplayFacts />) //no citation marks in the render method
-
+root.render(<DisplayFacts />); //no citation marks in the render method
