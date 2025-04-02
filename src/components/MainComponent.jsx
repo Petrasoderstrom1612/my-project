65085dc982332373049148fca3c4c22f62665369
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-04-02 17:05:58 +0200

    added background img

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,12 +1,14 @@
 
 export function MainComponent () {
     return(
+      <main>
         <ul>
-        <li>Was first release in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 200K stars on GitHub</li>
-        <li>Is maintained by Meta</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
+          <li>Was first release in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 200K stars on GitHub</li>
+          <li>Is maintained by Meta</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
       </ul>
+      </main>
     )
 }
